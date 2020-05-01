6fb2bcaa9c961f2afbe4e1a321d7d2f46565cba6
felapr1804 <63898749+felapr1804@users.noreply.github.com>
2020-05-02 01:21:12 +0530

    Dynamic telethon version
    Trying to print version dynamically

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,6 +1,7 @@
 """Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
 import asyncio
 from telethon import events
+from telethon import __version__
 from telethon.tl.types import ChannelParticipantsAdmins
 from platform import uname
 from userbot import ALIVE_NAME
@@ -11,7 +12,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`░█─░█ █▀▀ █── █── █▀▀█ \n░█▀▀█ █▀▀ █── █── █──█ \n░█─░█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ \n\nYes Sir ! I'm Alive\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
+    await alive.edit("`░█─░█ █▀▀ █── █── █▀▀█ \n░█▀▀█ █▀▀ █── █── █──█ \n░█─░█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ \n\nYes Sir ! I'm Alive\n\nTelethon version:" +str(__version__)+ "\nPython: 3.7.3\n\n`"
                     f"`My Master`: {DEFAULTUSER}\n"
                      "`My Owner`: @TechnoAyanOfficial\n\n"
                      "Join [Channel](https://t.me/TechnoAyanBoT/) For Latest Updates")
