9af75f2c6830009bd139b04ab7e39ce85bc80b92
felapr1804 <63898749+felapr1804@users.noreply.github.com>
2020-05-02 01:04:35 +0530

    Update and rename counth.py to count_10.py
    Useless counter to 10 for testing

---
+++ userbot/plugins/count_10.py
@@ -0,0 +1,47 @@
+# @ayushk780
+# Big Thanks To Spechide & @TechnoAyanBoT
+
+"""Counth: Avaible commands: .cten
+"""
+
+import asyncio
+from telethon import events
+from uniborg.util import admin_cmd
+
+
+
+@borg.on(admin_cmd(pattern=r"cten"))
+async def _(event):
+    if event.fwd_from:
+        return
+
+    animation_interval = 3
+
+    animation_ttl = range(0, 10)
+
+    # input_str = event.pattern_match.group(1)
+
+    # if input_str == "hack":
+
+    await event.edit("Printing count..")
+
+    animation_chars = [
+    
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
+    
+    ]
+
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+
+        await event.edit(animation_chars[i % 10])
