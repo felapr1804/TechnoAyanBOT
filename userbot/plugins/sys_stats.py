dd722f4b09fb35dce7daeec7826dd4f7147cf13b
felapr1804 <63898749+felapr1804@users.noreply.github.com>
2020-05-02 02:10:07 +0530

    Try to get bot Uptime

--- userbot/plugins/sys_stats.py
+++ userbot/plugins/sys_stats.py
@@ -1,14 +1,14 @@
-# @TechnoAyanBoT
-# Big Thanks To Spechide
+# @ayushk780
+# Big Thanks To Spechide and @TechnoAyanBoT
 
 """Counth: Avaible commands: .hstat
 """
 
 import asyncio
 from telethon import events
-from uniborg.util import admin_cmd, humanbytes
+from uniborg.util import admin_cmd, humanbytes,get_readable_time
 import shutil
-
+from userbot import botStartTime
 
 
 @borg.on(admin_cmd(pattern=r"hstat"))
@@ -16,11 +16,13 @@ async def _(event):
     if event.fwd_from:
         return
     
+    currentTime = get_readable_time((time.time() - botStartTime))
     total, used, free = shutil.disk_usage('.')
     total = humanbytes(total)
     used = humanbytes(used)
     free = humanbytes(free)
-    stats = f'Total disk space: {total}\n' \
+    stats = f'Bot Uptime: {currentTime}\n' \
+            f'Total disk space: {total}\n' \
                         f'Used: {used}\n' \
                         f'Free: {free}'
 
