74faac1aae1c350b0583a5e6405b414d6947162c
felapr1804 <63898749+felapr1804@users.noreply.github.com>
2020-05-02 02:43:49 +0530

    Finally bot stats are working
    Userbot's starts like its Up time and Disk usage.

--- userbot/plugins/bot_stats.py
+++ userbot/plugins/bot_stats.py
@@ -1,25 +1,28 @@
 # @ayushk780
 # Big Thanks To Spechide and @TechnoAyanBoT
 
-"""Counth: Avaible commands: .hstat
+"""Counth: Avaible commands: .bstats
 """
-
 import asyncio
 from telethon import events
-from uniborg.util import admin_cmd, humanbytes, get_readable_time
+from uniborg.util import admin_cmd, humanbytes,get_readable_time
 import shutil
+import time
+from userbot import botStartTime
 
 
-@borg.on(admin_cmd(pattern=r"hstat"))
+@borg.on(admin_cmd(pattern=r"bstats"))
 async def _(event):
     if event.fwd_from:
         return
     
+    currentTime = get_readable_time((time.time() - botStartTime))
     total, used, free = shutil.disk_usage('.')
     total = humanbytes(total)
     used = humanbytes(used)
     free = humanbytes(free)
-    stats = f'Total disk space: {total}\n' \
+    stats = f'Bot Uptime: {currentTime}\n' \
+            f'Total disk space: {total}\n' \
                         f'Used: {used}\n' \
                         f'Free: {free}'
 
