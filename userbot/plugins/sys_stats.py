45e918258be91d1e31a8135c7071fec43f1fa768
felapr1804 <63898749+felapr1804@users.noreply.github.com>
2020-05-02 02:21:03 +0530

    Update sys_stats.py

--- userbot/plugins/sys_stats.py
+++ userbot/plugins/sys_stats.py
@@ -6,9 +6,8 @@
 
 import asyncio
 from telethon import events
-from uniborg.util import admin_cmd, humanbytes,get_readable_time
+from uniborg.util import admin_cmd, humanbytes, get_readable_time
 import shutil
-from uniborg import botStartTime
 
 
 @borg.on(admin_cmd(pattern=r"hstat"))
@@ -16,13 +15,11 @@ async def _(event):
     if event.fwd_from:
         return
     
-    currentTime = get_readable_time((time.time() - botStartTime))
     total, used, free = shutil.disk_usage('.')
     total = humanbytes(total)
     used = humanbytes(used)
     free = humanbytes(free)
-    stats = f'Bot Uptime: {currentTime}\n' \
-            f'Total disk space: {total}\n' \
+    stats = f'Total disk space: {total}\n' \
                         f'Used: {used}\n' \
                         f'Free: {free}'
 
